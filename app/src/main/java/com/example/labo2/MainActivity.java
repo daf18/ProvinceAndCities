f7475cca3dd1aa18a96199e365f8c25ea54ebea3
Andreea <totyo_deea@yahoo.com>
2022-06-26 17:11:07 -0400

    spinners and flag change

--- app/src/main/java/com/example/labo2/MainActivity.java
+++ app/src/main/java/com/example/labo2/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.labo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             arrayList_sk,arrayList_alb,arrayList_tnl;
     ArrayAdapter<String> arrayAdapter_villes;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +37,16 @@ public class MainActivity extends AppCompatActivity {
         spinner_provinces = findViewById(R.id.spinner_provinces);
         spinner_villes = findViewById(R.id.spinner_villes);
 
+        ImageView img= (ImageView) findViewById(R.id.image);
+
         arrayList_provinces = new ArrayList<>();
-        arrayList_provinces.add("Québec");
+        arrayList_provinces.add("Quebec");
         arrayList_provinces.add("Ontario");
-        arrayList_provinces.add("Nouvelle-Écosse");
+        arrayList_provinces.add("Nouvelle-Ecosse");
         arrayList_provinces.add("Nouveau-Brunswick");
         arrayList_provinces.add("Manitoba");
         arrayList_provinces.add("Colombie-Britannique");
-        arrayList_provinces.add("Île-du-Prince-Édouard");
+        arrayList_provinces.add("Ile-du-Prince-Edouard");
         arrayList_provinces.add("Saskatchewan");
         arrayList_provinces.add("Alberta");
         arrayList_provinces.add("Terre-Neuve-et-Labrador");
@@ -69,5 +77,46 @@ public class MainActivity extends AppCompatActivity {
         arrayList_ns.add("New Glasgow");
         arrayList_ns.add("Glace Bay");
 
+        arrayList_nb = new ArrayList<>();
+        arrayList_nb.add("Moncton");
+        arrayList_nb.add("Saint John");
+        arrayList_nb.add("Fredericton");
+        arrayList_nb.add("Dieppe");
+        arrayList_nb.add("Riverview");
+
+        arrayList_mb = new ArrayList<>();
+        arrayList_mb.add("Winnipeg");
+        arrayList_mb.add("Brandon");
+        arrayList_mb.add("Steinbach");
+        arrayList_mb.add("Thompson");
+        arrayList_mb.add("Portage La Prairie");
+
+        // TODO: finish provinces
+
+        spinner_provinces.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                Toast.makeText(parent.getContext(),
+                         parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+
+                switch (parent.getItemAtPosition(pos).toString()) {
+                    case "Quebec":
+                        arrayAdapter_villes = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item,arrayList_qc);
+                        img.setImageResource(R.drawable.quebec);
+                        break;
+                    case "Ontario":
+                        arrayAdapter_villes = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item,arrayList_on);
+                        img.setImageResource(R.drawable.ontario);
+                        break;
+                }
+    spinner_villes.setAdapter(arrayAdapter_villes);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("selected", "onNothingSelected: ");
+            }
+        });
     }
 }
